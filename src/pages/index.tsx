a532939bc9289626683626686d7ced5bd4b2be84
Marie Bergounioux <marie@MacBook-Pro-de-Marie.local>
2024-03-29 22:03:17 +0100

    test color mainpage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,11 @@ import { css } from '../../styled-system/css';
 
 export default function Home() {
   return (
-    <div className={css({ color:'white', fontSize: "8xl", fontWeight: 'bold', background: 'url(https://langues.brolyus.com/_ipx/w_640,q_75/%2Fimages%2Fpage1.jpg?url=%2Fimages%2Fpage1.jpg&w=640&q=75) center no-repeat' ,backgroundColor: 'black', width: '100%', height: "calc(100% - 60px)" })}>Hello 🐼!</div>
+    <div className={css({ color:'white', fontSize: "2xl", fontWeight: 'bold', background: 'url(https://langues.brolyus.com/_ipx/w_640,q_75/%2Fimages%2Fpage1.jpg?url=%2Fimages%2Fpage1.jpg&w=640&q=75) center no-repeat' ,backgroundColor: 'black', width: '100%', height: "calc(100% - 60px)", display: 'flex', justifyContent: 'space-around' })}>
+      <p>Loup noir</p>
+      <p>Black wolf</p>
+      <p>Lobo nero</p>
+      <p>Lobo negro</p>
+    </div>
   )
 }
