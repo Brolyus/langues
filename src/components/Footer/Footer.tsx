5ab75cd8d310153d03f0ef2f14c9f125087125af
Anthony <desousanthony@gmail.com>
2024-03-29 12:54:52 +0100

    align footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import Arrow from '../Arrow/Arrow'
 
 const Footer: React.FC = () => {
     return (
-        <footer className={css({ backgroundColor: "mainBlue", height: "60px", display: "flex", justifyContent: "space-around", color: "mainYellow", borderTop: "1px solid token(colors.mainYellow)" })}>
+        <footer className={css({ backgroundColor: "mainBlue", height: "60px", display: "flex", justifyContent: "space-around", color: "mainYellow", borderTop: "1px solid token(colors.mainYellow)", alignItems: "center" })}>
             <Arrow className={css({ color: "mainYellow !important", fill: 'mainYellow', transform: 'scaleX(-1)' })} />
             <button className={css({ fontSize: '2xl' })} >&#127467;&#127479;</button>
             <button className={css({ fontSize: '2xl' })}>&#127477;&#127481;</button>
