71fc7cdd51c5237d5ddd954a2ebb6501dcc97017
Marie Bergounioux <marie@MacBook-Pro-de-Marie.local>
2024-03-29 21:56:53 +0100

    fix color & image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,6 @@ import { css } from '../../styled-system/css';
 
 export default function Home() {
   return (
-    <div className={css({ fontSize: "8xl", fontWeight: 'bold', backgroundColor: 'green', width: '100%', height: "calc(100% - 60px)" })}>Hello 🐼!</div>
+    <div className={css({ color:'white', fontSize: "8xl", fontWeight: 'bold', background: 'url(https://langues.brolyus.com/_ipx/w_640,q_75/%2Fimages%2Fpage1.jpg?url=%2Fimages%2Fpage1.jpg&w=640&q=75) center no-repeat' ,backgroundColor: 'black', width: '100%', height: "calc(100% - 60px)" })}>Hello 🐼!</div>
   )
 }
