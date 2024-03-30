3adf4a6dcba80c2237047d7f19c568025cd4b801
Marie Bergounioux <marie@MacBook-Pro-de-Marie.local>
2024-03-30 12:50:50 +0100

    test size & audio on main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,26 +23,26 @@ export default function Home() {
 
 
   return (
-    <div className={css({ color:'white', fontSize: "2xl", fontWeight: 'bold', background: 'url(https://langues.brolyus.com/_ipx/w_640,q_75/%2Fimages%2Fpage1.jpg?url=%2Fimages%2Fpage1.jpg&w=640&q=75) center no-repeat' ,backgroundColor: 'black', width: '100%', height: "calc(100% - 60px)", display: 'flex', justifyContent: 'space-around' })}>
-      <section>
+    <div className={css({ color:'white', fontSize: "md", fontWeight: 'bold', background: 'url(https://langues.brolyus.com/_ipx/w_640,q_75/%2Fimages%2Fpage1.jpg?url=%2Fimages%2Fpage1.jpg&w=640&q=75) center no-repeat' ,backgroundColor: 'black', width: '100%', height: "calc(100% - 60px)", display: 'flex', flexDirection: 'column', justifyContent: 'space-around' })}>
+      <section className={css({ display: 'flex', justifyContent: 'center', alignItems: 'center'})}>
         <p onClick={() => playAudio('/audio/fr.mp3')}>Loup noir</p>
         <p onClick={() => playAudio('/audio/ca.mp3')}>Black wolf</p>
         <p onClick={() => playAudio('/audio/fr.mp3')}>Lobo nero</p>
         <p onClick={() => playAudio('/audio/ca.mp3')}>Lobo negro</p>
       </section>
-      <section>
+      <section className={css({ display: 'flex', justifyContent: 'center', alignItems: 'center'})}>
         <p onClick={() => playAudio('/audio/fr.mp3')}>Loup noir</p>
         <p onClick={() => playAudio('/audio/ca.mp3')}>Black wolf</p>
         <p onClick={() => playAudio('/audio/fr.mp3')}>Lobo nero</p>
         <p onClick={() => playAudio('/audio/ca.mp3')}>Lobo negro</p>
       </section>
-      <section>
+      <section className={css({ display: 'flex', justifyContent: 'center', alignItems: 'center'})}>
         <p onClick={() => playAudio('/audio/ca.mp3')}>Loup noir</p>
         <p onClick={() => playAudio('/audio/fr.mp3')}>Black wolf</p>
         <p onClick={() => playAudio('/audio/ca.mp3')}>Lobo nero</p>
         <p onClick={() => playAudio('/audio/fr.mp3')}>Lobo negro</p>
       </section>
-      <section>
+      <section className={css({ display: 'flex', justifyContent: 'center', alignItems: 'center'})}>
         <p onClick={() => playAudio('/audio/ca.mp3')}>Loup noir</p>
         <p onClick={() => playAudio('/audio/fr.mp3')}>Black wolf</p>
         <p onClick={() => playAudio('/audio/ca.mp3')}>Lobo nero</p>
