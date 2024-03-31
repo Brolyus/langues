26dd2743875dbe312e0c642ad6a56521944950a9
Marie Bergounioux <marie@MacBook-Pro-de-Marie.local>
2024-03-31 19:30:02 +0200

    wip

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,7 +24,7 @@ export default function Home() {
 
   return (
     <div className={css({ color:'white', fontSize: "md", fontWeight: 'bold', background: 'url(https://langues.brolyus.com/_ipx/w_640,q_75/%2Fimages%2Fpage1.jpg?url=%2Fimages%2Fpage1.jpg&w=640&q=75) center no-repeat' ,backgroundColor: 'black', width: '100%', height: "calc(100% - 60px)", display: 'flex', flexDirection: 'column', justifyContent: 'space-around' })}>
-      <section className={css({ display: 'flex', justifyContent: 'center', alignItems: 'center'})}>
+      <section className={css({ display: 'flex', justifyContent: 'space-around', alignItems: 'center'})}>
         <p onClick={() => playAudio('/audio/fr.mp3')}>Loup noir</p>
         <p onClick={() => playAudio('/audio/ca.mp3')}>Black wolf</p>
         <p onClick={() => playAudio('/audio/fr.mp3')}>Lobo nero</p>
