da830c2a1ed2ab81b150b3f49b7522ab88835b05
Anthony <deousanthony@gmail.com>
2024-05-12 21:07:54 +0200

    add go home button

--- src/pages/remerciements.tsx
+++ src/pages/remerciements.tsx
@@ -3,10 +3,10 @@ import { polyfillCountryFlagEmojis } from 'country-flag-emoji-polyfill';
 import { css } from '../../styled-system/css';
 import thanksData from '@/tools/authors.json'
 import isoToEmoji from '@/tools/functions/isoToEmoji';
+import Link from 'next/link';
 
 
 export default function Thanks() {
-    console.log(thanksData)
     useEffect(() => {
         polyfillCountryFlagEmojis()
     }, [])
@@ -14,7 +14,7 @@ export default function Thanks() {
         <>
             <div className={css({ zIndex: 0, fontFamily: "Arial", height: "100%", display: "flex", alignItems: "center", padding: "5", flexDirection: "column", backgroundColor: '#DEE2E6', position: 'relative', overflow: 'auto', lg: { maxWidth: '860px', margin: 'auto', padding: '0 100px' } })}>
                 <h2 className={css({ fontSize: '2xl', borderBottom: '1px solid black', marginBottom: '20px' })}>
-                    Texte écrit, traduit et lu par les élèves de l'UPE2A du collège Anne Frank:
+                    Texte écrit, traduit et lu par les élèves de l&apos;UPE2A du collège Anne Frank:
                 </h2>
                 {thanksData?.map((element, index) => (
                     <section key={index} className={css({ display: 'flex', alignItems: 'center', width: '100%', justifyContent: 'flex-start', padding: '5px' })}>
@@ -26,10 +26,14 @@ export default function Thanks() {
                         <span className={css({ margin: '0 5px' })}>-</span>
                         <p className={css({ fontSize: '2xl', textTransform: 'lowercase' })}>{element.language}</p>
                     </section>
-
                 ))
                 }
-            </div>
+                <Link href='/'>
+                    <button className={css({ display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: 'xl', backgroundColor: 'white', borderRadius: '5px', padding: '3', width: '200px', marginTop: '50px' })}>
+                        Retour à l&apos;accueil
+                    </button>
+                </Link>
+            </div >
         </>
     )
 }
